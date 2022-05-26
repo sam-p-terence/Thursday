3e39aecbf3e07914204e8cb782ef35f05a56b226
Terence Sam <sam.p.terence@devmountain.com>
2022-05-26 16:43:18 -0600

    complete md2 lab

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -164,7 +164,7 @@ for (var i = letters.length - 1; i >= 0; i--)
 // Switch statements can be excellent alternatives to if blocks. Look up switch statements (I recommend W3 Schools) and try to implement one for the following.
 
 // Do not edit the code below.
-let letterGrade = 'B'
+const letterGrade = 'F'
 // Do not edit the code above.
 
 /* Use a switch statement on 'letterGrade' and console.log the appropriate response.
@@ -179,7 +179,22 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 
 //Code Here
 
-
+switch(letterGrade) {
+	case "A":
+		console.log("The student is doing excellently.");
+		break;
+	case "B":
+		console.log("The student is doing well.");
+		break;
+	case "C":
+		console.log("The student is doing alright.");
+		break;
+	case "D":
+		console.log("The student is not doing very well.");
+		break;
+	case "F":
+		console.log("The student is failing.");
+}
 
 ////////// PROBLEM 11 //////////
 /* The famous FizzBuzz, Devmountain style!
@@ -206,3 +221,14 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+
+for (let i = 1; i <= 100; i++) 
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("Devmountain")
+  } else if (i % 5 === 0) {
+    console.log("mountain")
+  } else if (i % 3 === 0) {
+    console.log("Dev")
+  } else {
+    console.log(i);
+  }
